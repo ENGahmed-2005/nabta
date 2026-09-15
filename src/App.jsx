aec01d9a5cd53ebd60b387f1a31c7aa4b03d8cf5
ENG Ahmed Alkahlout <ahmedkahlout258@gmail.com>
2026-09-15 15:34:35 +0300

    feat: wire app language and theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import Home from "./pages/Home.jsx";
+import { AppProvider } from "./context/AppContext.jsx";
 
 export default function App() {
-  return <Home />;
+  return (
+    <AppProvider>
+      <Home />
+    </AppProvider>
+  );
 }
